2bc4845940b119cdf107cf1ff7345a27ffa8818f
Alejandro Serrano <40070068+Andresser17@users.noreply.github.com>
2022-08-02 09:20:23 -0400

    Island (#1)
    * Fix bug in InputTag.js
    
    * style(Island): progress bar width is now dinamic
    
    progress bar width now dependent in percent prop
    
    Co-authored-by: Andresser17 <tensasangetsu1254@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import "./App.css";
 // import { useState } from "react";
-import InputTag from "./InputTag";
-import Input from "./Input";
+import Island from "./Island";
+// Icons
+import { ReactComponent as ErrorIcon } from "./icons/error-icon.svg";
 
 function App() {
-  const handleData = (tags, setData) => {
-    if (tags.length <= 3) {
-      setData(undefined, tags)
-    }
-  };
-
   return (
-    <div className="App p-4 h-screen bg-blue-600 flex items-start justify-center">
-      <div className="w-96">
-        <InputTag
-          getData={handleData}
-          type="text"
-          placeholder="Your name"
-          description="You can put your name"
-          palette="success"
-        />
-        <div className="mt-10">
-          <Input
-            getData={handleData}
-            type="text"
-            placeholder="Your name"
-            readOnly
-            description="You can put your name"
-          />
-        </div>
-      </div>
+    <div className="App p-4 h-screen bg-green-300 flex items-start justify-center">
+      <Island
+        title="Your text"
+        head="Head"
+        description="An easy description"
+        mode="pending"
+        percent={50}
+      >
+        {/* {{ */}
+        {/*   icon: <ErrorIcon className="w-8 ml-6 mb-4" />, */}
+        {/*   params: [ */}
+        {/*     { text: "Params", icon: <ErrorIcon className="w-6" /> }, */}
+        {/*     { text: "Params", icon: <ErrorIcon className="w-6" /> }, */}
+        {/*   ], */}
+        {/* }} */}
+      </Island>
     </div>
   );
 }
