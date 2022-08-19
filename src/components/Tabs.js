b8a4512fe51e5755073f1442691b2bc5c1529ae5
Andresser17 <andresserserrano2020@gmail.com>
2022-08-19 11:39:58 -0400

    feat: add Tabs.stories.js
    fix Tabs components border styles

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,60 +1,55 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-// Helpers
-import formatKey from "helpers/key";
 
-function Tab({ text, size, id, horizontal, selected, onSelected }) {
-  const font =
+function Tab({ tab, size, vertical, selected, onSelected }) {
+  const hoverStyle = `al-border-solid ${
+    selected === tab.value
+      ? "al-border-bg hover:al-border-bg"
+      : "al-border-black/0 hover:al-border-zinc-500"
+  }`;
+  const verticalStyle = vertical
+    ? "al-border-l-2 al-pl-5 al-py-2"
+    : "al-border-b-2 al-px-4 al-py-3";
+  const fontStyle =
     size === "sm" ? "al-text-sm" : size === "md" ? "al-text-md" : "al-text-lg";
 
   return (
     <span
-      onClick={onSelected}
-      className={`al-inline-block al-text-zinc-500 hover:al-border-zinc-500 al-cursor-pointer ${
-        selected === id ? "al-border-bg" : "al-border-black/0"
-      } ${font} ${
-        horizontal ? "al-border-l-2 al-pl-5 al-py-2" : "al-border-b-2 al-px-4 al-py-3"
-      }`}
+      onClick={() => onSelected(tab.value)}
+      className={`al-inline-block al-text-text al-cursor-pointer al-border-0 ${fontStyle} ${hoverStyle} ${verticalStyle}`}
     >
-      {text}
+      {tab.label}
     </span>
   );
 }
 
 function Tabs({
   palette = "primary",
-  tabs,
+  tabs = [],
   size = "sm",
-  horizontal,
-  getSelected,
+  vertical,
+  onClick = (value) => undefined,
 }) {
   const [selected, setSelected] = useState("");
 
   // if one tab is clicked, update selected
-  const handleSelected = (key) => {
-    setSelected(key);
+  const handleSelected = (tab) => {
+    setSelected(tab);
     // pass selected tab to parent
-    getSelected(key);
+    onClick(tab);
   };
 
   return (
-    <div className={`${horizontal ? "al-flex al-flex-col" : ""} ${palette}`}>
+    <div className={`${vertical ? "al-flex al-flex-col" : ""} ${palette}`}>
       {tabs &&
-        tabs.map((t, i) => {
-          const key = formatKey(t);
-
-          // select the first tab by default
-          if (i === 0 && selected.length === 0) setSelected(key);
+        tabs.map((tab, i) => {
+          if (selected.length === 0 && tab.selected) setSelected(tab.value);
 
           return (
             <Tab
-              key={key}
-              id={key}
-              size={size}
-              text={t}
-              selected={selected}
-              onSelected={() => handleSelected(key)}
-              horizontal={horizontal}
+              key={tab.value}
+              onSelected={handleSelected}
+              {...{ tab, vertical, selected, size, selected }}
             />
           );
         })}
@@ -65,8 +60,8 @@ Tabs.propTypes = {
   palette: PropTypes.string,
   tabs: PropTypes.array,
   size: PropTypes.string,
-  horizontal: PropTypes.bool,
-  getSelected: PropTypes.func,
+  vertical: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default Tabs;
