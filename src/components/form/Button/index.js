6385e8f33f3219ec39c5000d327362d9bfbc8e57
Andresser17 <andresserserrano2020@gmail.com>
2022-10-27 18:50:56 -0400

    feat: add useStyles hook and Button/Container
    refactor Button component

--- src/components/form/Button/index.js
+++ src/components/form/Button/index.js
@@ -1,58 +1,49 @@
 import React, { useMemo } from "react";
+import useStyles from "hooks/useStyles";
 // Components
+import Container from "./Container";
 import Icon from "./Icon";
 import Circle from "./Circle";
-// Icons
-import { AiOutlineLoading3Quarters as LoadingIcon } from "react-icons/ai";
 import PropTypes from "prop-types";
 
 function Button({
-  theme,
+  theme = "primary",
   text,
-  border,
-  bold,
-  rounded,
   disabled,
   loading,
+  styles,
   onClick,
 }) {
-  const className = useMemo(() => {
-    const styles = {
-      main: `flex items-center cursor-pointer bg-primary text-text ${theme}`,
+  const className = useStyles(
+    {
+      height: "h-9",
       hover: "hover:bg-primary/90",
       active: "active:bg-primary/80",
       focus:
         "focus:outline focus:outline-1 focus:bg-primary/70 focus:outline-border",
-      bold: bold ? "font-semibold" : "font-normal",
-      rounded: rounded ? "rounded-md" : "rounded-sm",
+      disabled: "disabled:bg-primary/50 disabled:cursor-auto",
+      rounded: "rounded-sm",
       padding: !text ? "p-2" : "px-5 py-[0.3rem]",
-      disabled: "disabled:bg-primary/50",
-      border: border ? "border-solid border border-border" : "border-none",
-    };
-
-    return Object.keys(styles)
-      .map((key) => styles[key])
-      .join(" ");
-  }, [theme, bold, rounded, text, border]);
+      border: "border-none",
+    },
+    {
+      main: `cursor-pointer bg-primary text-text relative ${theme}`,
+    },
+    styles
+  );
 
   return (
-    <button {...{ onClick, disabled, className }}>
-      {loading ? (
-        <LoadingIcon className="text-lg block w-5 h-5 animate-spin" />
-      ) : (
-        text && <span className="text-lg block">{text}</span>
-      )}
-    </button>
+    <Container {...{ disabled, loading, className, onClick }}>
+      <span className="text-lg block">{text}</span>
+    </Container>
   );
 }
 Button.propTypes = {
-  palette: PropTypes.string,
+  theme: PropTypes.string,
   text: PropTypes.string,
-  rounded: PropTypes.bool,
-  border: PropTypes.bool,
-  bold: PropTypes.bool,
   loading: PropTypes.bool,
   disabled: PropTypes.bool,
+  styles: PropTypes.object,
   onClick: PropTypes.func,
   children: PropTypes.element,
 };
